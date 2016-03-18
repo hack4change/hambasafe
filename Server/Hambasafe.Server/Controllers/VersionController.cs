a1fbc52af581fc57377ae5d2425ee9a126ec73b8
Nigel Basel <nigel@jwayela.com>
2016-03-19 00:28:08 +0200

    Added Server Starting Point Solution

---
+++ Server/Hambasafe.Server/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hambasafe.Server.Controllers
+{
+    public class VersionController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+    }
+}
---
+++ Server/Hambasafe.Server/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hambasafe.Server.WebApiApplication" Language="C#" %>
