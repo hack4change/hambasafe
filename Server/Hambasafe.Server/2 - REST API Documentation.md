425610faa400c274db88c2f62e027ddbbbddf1be
Peter Wood <ettery@gmail.com>
2016-03-19 18:19:20 +0200

    API doc

---
+++ Server/Hambasafe.Server/2 - REST API Documentation.md
@@ -0,0 +1,32 @@
+﻿#REST API Documentation
+
+##Major Entities
+
+###Users
+
+- `/v1/users/`
+- `/v1/users?id=1`
+- `/v1/users?username="peter"`
+
+
+###Events
+
+- `/v1/events/`
+- `/v1/events?id=1`
+- `/v1/events?created-by-user="peter"`  Will list the events created by this user
+
+
+
+
+##Attributes/Types
+
+###EventType
+
+- `/v1/eventtypes`
+
+
+###Address Attributes
+
+- `/v1/provinces`
+- `/v1/cities?province="Western Cape"`
+- `/v1/suburbs?city="Cape Town"`
--- Server/Hambasafe.Server/Controllers/v1/EventsController.cs
+++ Server/Hambasafe.Server/Controllers/v1/EventsController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Hambasafe.Server.Services.Configuration;
 using Hambasafe.Server.Services.TableStorage;
@@ -15,6 +14,41 @@ namespace Hambasafe.Server.Controllers.v1
         public EventsController(IConfigurationService configuration, ITableStorageService tableStorage) :
             base(configuration, tableStorage)
         {
+        }
+        [AllowAnonymous]
+        [Route("events"), HttpGet]
+        public async Task<HttpResponseMessage> GetEvents()
+        {
+            try
+            {
+                var dummyEvents = new[]
+                {
+                    new { Id = 1, Name = "Event 1", Type="Trail Run" },
+                    new { Id = 2, Name = "Event 2", Type="MTB" }
+                };
+
+                return Request.CreateResponse(HttpStatusCode.OK, dummyEvents);
+            }
+            catch (Exception error)
+            {
+                return HandleError(error);
+            }
+        }
+
+        [AllowAnonymous]
+        [Route("event"), HttpGet]
+        public async Task<HttpResponseMessage> GetEvent(int id)
+        {
+            try
+            {
+                var dummyEvent = new { Id = 1, Name = "Event 1", Type = "Trail Run" };
+
+                return Request.CreateResponse(HttpStatusCode.OK, dummyEvent);
+            }
+            catch (Exception error)
+            {
+                return HandleError(error);
+            }
         }
     }
 }
