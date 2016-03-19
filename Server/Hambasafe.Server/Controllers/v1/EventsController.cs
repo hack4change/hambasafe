12361a48192d486d56a6f817915126f39762af53
BastAiluros <bast.ailuros@gmail.com>
2016-03-19 16:34:43 +0200

    Added EventsController

---
+++ Server/Hambasafe.Server/Controllers/v1/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Hambasafe.Server.Services.Configuration;
+using Hambasafe.Server.Services.TableStorage;
+
+namespace Hambasafe.Server.Controllers.v1
+{
+    [RoutePrefix("v1")]
+    public class EventsController : ApiControllerBase
+    {
+        public EventsController(IConfigurationService configuration, ITableStorageService tableStorage) :
+            base(configuration, tableStorage)
+        {
+        }
+    }
+}
