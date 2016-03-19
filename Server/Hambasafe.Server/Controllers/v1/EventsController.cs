36d5f1d61c9098d06e7744ac9f4b3efa24f1f84a
BastAiluros <bast.ailuros@gmail.com>
2016-03-19 17:45:37 +0200

    Added view models for Event, EventType, Suburb, City, Province

--- Server/Hambasafe.Server/Controllers/v1/EventsController.cs
+++ Server/Hambasafe.Server/Controllers/v1/EventsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Threading.Tasks;
 using Hambasafe.Server.Services.Configuration;
 using Hambasafe.Server.Services.TableStorage;
 
@@ -15,6 +16,27 @@ namespace Hambasafe.Server.Controllers.v1
         public EventsController(IConfigurationService configuration, ITableStorageService tableStorage) :
             base(configuration, tableStorage)
         {
+        }
+
+        [AllowAnonymous]
+        [Route("events"), HttpGet]
+        public async Task<HttpResponseMessage> GetEvents()
+        {
+            try
+            {
+                var dummyEvents = new[]
+                {
+                    new { Id = 1, Name = "Event 1"},
+                    new { Id = 2, Name = "Event 2"},
+                    new {Id = 3, Name = "Event 3"}
+                };
+
+                return Request.CreateResponse(HttpStatusCode.OK, dummyEvents);
+            }
+            catch (Exception error)
+            {
+                return HandleError(error);
+            }
         }
     }
 }
