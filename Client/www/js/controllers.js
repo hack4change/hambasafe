0d8972ec1f0a2dbcef4b5af6b587dccf80465065
Abdul <abdul@playlogix.com>
2016-03-19 11:28:39 +0200

    Puling user credentials and pupulating in temporary sign up form.

--- Client/www/js/controllers.js
+++ Client/www/js/controllers.js
@@ -21,6 +21,38 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('LandingCtrl', function($scope, $stateParams, Facebook, LocalStorage, $location) {
+  $scope.getLoginStatus = function() {
+    Facebook.getLoginStatus(function(response) {
+      if(response.status === 'connected') {
+        $scope.loggedIn = true;
+        $location.path('registration')
+      } else {
+        $scope.loggedIn = false;
+      }
+    });
+  };
+
+  $scope.fbLogin = function () {
+    // From now on you can use the Facebook service just as Facebook api says
+    Facebook.login(function (response) {
+      // Do something with response.
+      console.log(response);
+      LocalStorage.facebookAuth = response;
+    });
+  };
+})
+
+.controller('RegistrationCtrl', function($scope, $stateParams, Facebook, LocalStorage) {
+    $scope.fbMe = function() {
+    // From now on you can use the Facebook service just as Facebook api says
+      Facebook.api('/me', function(response) {
+        $scope.user = response;
+        console.log($scope.user);
+      });
+  };
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
