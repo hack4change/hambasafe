48a4e520f3aa23a49119a87fc0b88bec0a19e64e
Renier van Coller (Santam) <S945773@mud.internal.co.za>
2016-03-20 04:47:38 +0200

    broken add button

--- Client/www/js/controllers/HomeCtrl.js
+++ Client/www/js/controllers/HomeCtrl.js
@@ -1,10 +1,20 @@
-starterControllers.controller('HomeCtrl', function ($scope, eventFactory) {
+starterControllers.controller('HomeCtrl', function ($scope,  $location, eventFactory) {
     var t = eventFactory.get();
 
     $scope.events = [];
 
+	console.log("hello")
+
     for (var i = 0; i < 50; i++) {
       var event = {label: "Test " + i};
       $scope.events.push(event)
+    }
+
+    console.log("hello")
+
+
+    $scope.goCreate = function() {
+    	console.log("here")
+    	$location.path("create");
     }
   });
