fcb936a4317dd3593462e6575c287496e73f7524
JenMatlock <bast.ailuros@gmail.com>
2016-03-20 13:47:57 +0200

    Removed unused models and added event code

--- Server/Hambasafe.Server/Models/v1/EventModel.cs
+++ Server/Hambasafe.Server/Models/v1/EventModel.cs
@@ -1,3 +1,60 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Entities = Hambasafe.DataAccess.Entities;
+
+namespace Hambasafe.Server.Models.v1
+{
+    public class EventModel
+    {
+        public EventModel(Entities.Event dbEvent)
+        {
+            EventId = dbEvent.EventId;
+            Name = dbEvent.Name;
+            Description = dbEvent.Description;
+            EventType = new EventTypeModel(dbEvent.EventType);
+            EventDateTimeStart = dbEvent.DateTimeStart;
+            EventDateTimeEnd = dbEvent.DateTimeEnd;
+            Attributes = dbEvent.Attributes;
+            WaitMins = dbEvent.MaxWaitingMinutes;
+            PublicEvent = dbEvent.IsPublic;
+            StartLocation = new EventLocation(dbEvent.EventLocation);
+            EndLocation = dbEvent.EventLocation1 == null ? null : new EventLocation(dbEvent.EventLocation1);
+            OwnerUser = new UserModel(dbEvent.User);
+        }
+
+        public EventModel()
+        {
+        }
+
+        public int EventId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public EventTypeModel EventType { get; set; }
+
+        public DateTime EventDateTimeStart { get; set; }
+
+        public DateTime? EventDateTimeEnd { get; set; }
+
+        public string Attributes { get; set; }
+
+        public short WaitMins { get; set; }
+
+        public bool? PublicEvent { get; set; }
+
+        public EventLocation StartLocation { get; set; }
+
+        public EventLocation EndLocation { get; set; }
+
+        public UserModel OwnerUser { get; set; }
+    }
+=======
 ﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,4 +101,5 @@ namespace Hambasafe.Server.Models.v1
 
         public bool? PublicEvent { get; set; }
     }
+>>>>>>> origin/master
 }
