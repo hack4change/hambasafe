53f68a66b3d5228000e8e17954be1082eb7d3ede
MsEmma <emmaphiri@hotmail.com>
2016-03-20 15:21:27 +0200

    styled rating

--- Client/www/js/controllers/RatingCtrl.js
+++ Client/www/js/controllers/RatingCtrl.js
@@ -1,9 +1,9 @@
 starterControllers.controller('RatingCtrl', function ($scope) {
-  $scope.event = {Name:"test",Description:"test1"}
+  $scope.event = {Name:"Event Name",Description:"Description"}
   $scope.Rate=3;
   $scope.setRate = function(val){
     $scope.Rate = val;
   };
-  
+
   console.log('Here');
 });
