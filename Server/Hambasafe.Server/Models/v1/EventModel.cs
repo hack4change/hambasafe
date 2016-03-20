50b5b996c50337e37ee823fc00599db871d5d146
JenMatlock <bast.ailuros@gmail.com>
2016-03-20 13:52:55 +0200

    Fixed issues

--- Server/Hambasafe.Server/Models/v1/EventModel.cs
+++ Server/Hambasafe.Server/Models/v1/EventModel.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -54,52 +53,4 @@ namespace Hambasafe.Server.Models.v1
 
         public UserModel OwnerUser { get; set; }
     }
-=======
-﻿﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using Entities = Hambasafe.DataAccess.Entities;
-
-namespace Hambasafe.Server.Models.v1
-{
-    public class EventModel
-    {
-        public EventModel(Entities.Event dbEvent)
-        {
-            EventId = dbEvent.EventId;
-            Name = dbEvent.Name;
-            Description = dbEvent.Description;
-            EventType = new EventTypeModel(dbEvent.EventType);
-            EventDateTimeStart = dbEvent.DateTimeStart;
-            EventDateTimeEnd = dbEvent.DateTimeEnd;
-            Attributes = dbEvent.Attributes;
-            WaitMins = dbEvent.MaxWaitingMinutes;
-            PublicEvent = dbEvent.IsPublic;
-        }
-
-        public EventModel()
-        {
-        }
-
-        public int EventId { get; set; }
-
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public EventTypeModel EventType { get; set; }
-
-        public DateTime EventDateTimeStart { get; set; }
-
-        public DateTime? EventDateTimeEnd { get; set; }
-
-        public object Attributes { get; set; }
-
-        public int WaitMins { get; set; }
-
-        public bool? PublicEvent { get; set; }
-    }
->>>>>>> origin/master
 }
