e748917c5c19ff7bb13467e4142be96e667baf02
BlydeLameco <blyde@lameco.co.za>
2016-03-20 09:58:45 +0200

    Begun integrating registration logic.

--- Server/Hambasafe.Server/Controllers/v1/AuthenticationController.cs
+++ Server/Hambasafe.Server/Controllers/v1/AuthenticationController.cs
@@ -7,10 +7,13 @@ using System.Web.Http;
 using Hambasafe.Server.Models.v1;
 using Hambasafe.Server.Services.Configuration;
 using Hambasafe.Server.Services.TableStorage;
+using Hambasafe.DataAccess.Entities;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace Hambasafe.Server.Controllers.v1
 {
-    [RoutePrefix("v1/authentication")]
+    [RoutePrefix("v1")]
     public class AuthenticationController : ApiControllerBase
     {
         public AuthenticationController(IConfigurationService configuration, ITableStorageService tableStorage) :
@@ -20,10 +23,40 @@ namespace Hambasafe.Server.Controllers.v1
 
         [AllowAnonymous]
         [Route("register"), HttpGet]
-        public async Task<HttpResponseMessage> Register()
+        public async Task<HttpResponseMessage> Register(RegisterModel registerModel)
         {
             try
             {
+                var dataContext = new HambasafeDataContext();
+
+                // Create new user
+                var User = new User()
+                {
+                    FirstNames = registerModel.FirstNames,
+                    LastName = registerModel.LastName,
+                    Gender = registerModel.Gender,
+                    DateOfBirth = registerModel.DateOfBirth,
+                    Status = "created",
+                    MobileNumber = registerModel.MobileNumber,
+                    EmailAddress = registerModel.EmailAddress,
+                    DateCreated = new DateTime(),
+                    DateLastLogin = new DateTime()
+                };
+
+                // Create the token
+                var key = Encoding.UTF8.GetBytes(registerModel.Password.ToUpper());
+                string hashString;
+
+                using (var hmac = new HMACSHA256(key))
+                {
+                    var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes());
+                    hashString = Convert.ToBase64String(hash);
+                }
+
+                return hashString;
+
+
+
                 return Request.CreateResponse(HttpStatusCode.OK);
             }
             catch (Exception error)
