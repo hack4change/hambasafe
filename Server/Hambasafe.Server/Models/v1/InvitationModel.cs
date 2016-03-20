d122e4e9d108bb0d889aa516e4e3090e9cefac8c
Waynne Human <waynne@gmail.com>
2016-03-20 12:13:09 +0200

    Added InvitationModel

---
+++ Server/Hambasafe.Server/Models/v1/InvitationModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Entities = Hambasafe.DataAccess.Entities;
+
+namespace Hambasafe.Server.Models.v1
+{
+    public class InvitationModel
+    {
+        public InvitationModel()
+        {
+        }
+
+        public InvitationModel(Entities.Invitation dbInvitation)
+        {
+            InvitationId = dbInvitation.InvitationId;
+            InvitorUserId = dbInvitation.InvitorUserId;
+            InviteeUserId = dbInvitation.InviteeUserId;
+            EventId = dbInvitation.EventId;
+            OptionalEmailInvitee = dbInvitation.OptionalEmailInvitee;
+            Status = dbInvitation.Status;
+            Comment = dbInvitation.Comment;
+        }
+
+        public int? InvitationId { get; set; }
+
+        public int? InvitorUserId { get; set; }
+
+        public int? InviteeUserId { get; set; }
+
+        public int? EventId { get; set; }
+
+        public string OptionalEmailInvitee { get; set; }
+
+        public string Status { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
