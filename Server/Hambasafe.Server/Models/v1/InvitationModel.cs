0dd543001f61f3ff7bdd84f4b9d366cfaab231a4
Waynne Human <waynne@gmail.com>
2016-03-20 12:14:03 +0200

    Cleaned up references

--- Server/Hambasafe.Server/Models/v1/InvitationModel.cs
+++ Server/Hambasafe.Server/Models/v1/InvitationModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Entities = Hambasafe.DataAccess.Entities;
+﻿using Entities = Hambasafe.DataAccess.Entities;
 
 namespace Hambasafe.Server.Models.v1
 {
