c459cd23ee2157f16a01249cd62a007b2d61f716
madangryscientist <madangryscientist@gmail.com>
2016-04-05 15:30:52 +0200

    more unit testing attempts

--- Server/Prebuild.ps1
+++ Server/Prebuild.ps1
@@ -24,4 +24,4 @@ else
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
 Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
 dnu restore
-& dnx -p "Hambasafe.Services.Tests\" test -xml TestResults.xml
+& dnx -p "Hambasafe.Services.Tests\" test 
