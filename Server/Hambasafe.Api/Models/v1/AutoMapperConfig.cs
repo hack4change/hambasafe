b43f02b997fa56f78fad56c588779d13cb2dc3d3
Waynne Human <waynne@gmail.com>
2016-04-11 00:18:48 +0200

    Wired up two-way mappers

--- Server/Hambasafe.Api/Models/v1/AutoMapperConfig.cs
+++ Server/Hambasafe.Api/Models/v1/AutoMapperConfig.cs
@@ -7,11 +7,16 @@ namespace Hambasafe.Api.Models.v1
         protected override void Configure()
         {
             CreateMap<Event, EventModel>();
+            CreateMap<EventModel, Event>();
+
             CreateMap<EventLocation, EventLocationModel>();
+            CreateMap<EventLocationModel, EventLocation>();
+
             CreateMap<EventType, EventTypeModel>();
+            CreateMap<EventTypeModel, EventType>();
+
             CreateMap<User, UserModel>();
-
-
+            CreateMap<UserModel, User>();
         }
     }
 }
