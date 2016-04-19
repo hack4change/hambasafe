59b710f8caeb6151ad260fdcc1dcef9b8814ae01
Waynne Human <waynne@gmail.com>
2016-04-19 21:32:07 +0200

    No longer need CoordinateModel

--- Server/Hambasafe.Api/Models/v1/CoordinateModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hambasafe.Api.Models.v1
-{
-    public class CoordinateModel
-    {
-        public double? Latitude { get; set; }
-        public double? Longitude { get; set; }
-        public double? Distance { get; set; }
-    }
-}
